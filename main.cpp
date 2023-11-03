00047fcf36de2d1897c43cbd89f3113c5b371a90
Julien <Jfabregoule@gaming.tech>
2023-11-03 02:52:25 +0100

    Music added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <conio.h>
 #include <Windows.h>
+#include <sdl_mixer.h>
 #include "includes/Window.h"
 #include "includes/Map.h"
 #include "includes/Game.h"
@@ -12,7 +13,16 @@ int	main(int argc, char** argv)
 	srand((unsigned int)time(0));;
 	Window	window;
 	window.map->isWon = false;
+	int init = Mix_Init(0);
 
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
+
+	Mix_Chunk* music = Mix_LoadWAV("rsrc/music.wav");
+	if (!music)
+	{
+		std::cout << "Music Error: " << Mix_GetError() << std::endl;
+	}
+	Mix_PlayChannel(-1, music ,-1);
 	while (window.running)
 	{
 		window.starting_tick = SDL_GetTicks();
